7ee6c3d23ac035eaa01f06c2ac59d4fa3fb56cb7
guoyin.chen <guoyin.chen@freescale.com>
2010-12-29 07:24:01 +0800

    ENGR00137445 Update Android.mk to add LOCAL_MODULE_TAGS := optional
    Update Android.mk to add LOCAL_MODULE_TAGS := optional
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- android-arm/bin/Android.mk
+++ android-arm/bin/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := bash
-LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES := bash
 include $(BUILD_PREBUILT)
--- linux-x86/ubifs/Android.mk
+++ linux-x86/ubifs/Android.mk
@@ -6,5 +6,7 @@ ifneq ($(BUILD_MKFS_UBIFS),true)
 
 LOCAL_PREBUILT_EXECUTABLES := mkfs.ubifs
 
+LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_HOST_PREBUILT)
 endif
